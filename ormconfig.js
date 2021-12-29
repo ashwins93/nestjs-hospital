efef801a918cff23f4147476076373166782b67c
Ashwin Sundararajan <aswinjeganath@gmail.com>
2021-12-29 20:09:14 +0530

    prod config

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,13 @@
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'dev',
-  password: 'password',
-  database: 'hospitals',
+  // host: 'localhost',
+  // port: 5432,
+  // username: 'dev',
+  // password: 'password',
+  // database: 'hospitals',
+  url:
+    process.env.DATABASE_URL ||
+    'postgres://dev:password@localhost:5432/hospitals',
   entities: ['dist/**/*.entity.js'],
   logging: true,
 };
--- src/auth/jwt.auth.guard.ts
+++ src/auth/jwt.auth.guard.ts
@@ -1,11 +1,5 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    console.log(request.headers.authorization);
-    return super.canActivate(context);
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
